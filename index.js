d0d34e5c07c8febb1825c36e071b7763f649bb9d
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-05-23 16:29:08 -0500

    commit tree 3b99cafb2ffe77dfc7362126ff565d1814be50ae

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+// function findMatching(str, drivers) {
+//   if (str === str) {
+//   drivers.filter(findMatching)
+//   return str;
+//   return filtered
+// } else {
+
+// }
+  
+// }
+
+// const result = drivers.filter(findMatching);
+
+// function findMatching(str) {
+//   str === str 
+// }
+
+
+// function findMatching(drivers, function(str) {
+//   return drivers;
+//   }
+
+function findMatching(drivers, str) {
+  
+const maybe = drivers.filter(driver => str.toLowerCase() === driver.toLowerCase())
+
+return maybe;
+}
+
+
+
+function fuzzyMatch(drivers, str) {
+
+const maybe = drivers.filter(driver => str === driver.slice(0,3));
+
+return maybe;
+}
+
+function matchName() {
+  //dot or bracket notation to access the information stored within the object stored within the array
+}
